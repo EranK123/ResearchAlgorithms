3ce1031391f7e37664993cd804730e88c699ff03
erankatz <eranki.katz@gmail.com>
2022-11-01 16:50:09 +0200

    finished 1 and 2

--- Ex1/Q1.py
+++ Ex1/Q1.py
@@ -1,4 +1,4 @@
-def safe_call(func, x, y, z):
+def safe_call(func, **kwargs):
     """
         >>> safe_call(f1,x=5,y=7.0,z=3)
         15.0
@@ -10,6 +10,8 @@ def safe_call(func, x, y, z):
         Traceback (most recent call last):
             ...
         Exception: Wrong Type
+        >>> safe_call(f3,x=5,y=7.1)
+        12.1
         >>> safe_call(f2, x="a", y="dwa", z="abc")
         'adwaabc'
         >>> safe_call(f2, x=2, y="dwa", z=2)
@@ -17,15 +19,14 @@ def safe_call(func, x, y, z):
             ...
         Exception: Wrong Type
     """
-    check_ano(func, x, y, z)  # call to a function which will check if all the types are as they are
-    return func(x, y, z)  # if everything is as it is we will return
+    check_ano(func, **kwargs)  # call to a function which will check if all the types are as they are
+    return func(**kwargs)  # if everything is as it is we will return
 
 
-def check_ano(func, x, y, z):
+def check_ano(func, **kwargs):
     annotations = func.__annotations__  # get the annotations of the attributes
-    localvars = locals()  # get all the all the local variables and symbols
     for x in annotations.keys():
-        if (type(localvars[x])) is not annotations[x]:  # check if they match
+        if (type(kwargs[x])) is not annotations[x]:  # check if they match
             raise Exception("Wrong Type")
 
 
@@ -37,11 +38,16 @@ def f2(x, y: str, z: str):
     return x + y + z
 
 
+def f3(x: int, y: float):
+    return x + y
+
+
 # running examples
 print(safe_call(f1, x=5, y=2.0, z=3))
 print(safe_call(f2, x="da", y="abc", z="3"))
+print(safe_call(f3, x=3, y=2.1))
 
-if __name__ == "__main__":
-    import doctest
-
-    print(doctest.testmod())
+# if __name__ == "__main__":
+#     import doctest
+#
+#     print(doctest.testmod())
