79f9194454af33c24b70d39a73c31716cbf9acf5
erankatz <eranki.katz@gmail.com>
2022-11-29 16:47:47 +0200

    finished 1 and 2

--- Ex5/Q1.py
+++ Ex5/Q1.py
@@ -1,47 +1,22 @@
 import itertools
 
 
-#
-# class BoundedSubsets:
-#     def __init__(self, bounded_set, val):
-#         self.set = bounded_set
-#         self.state = []
-#         self.val = val
-#
-#     def __iter__(self):
-#         return self
-#
-#     def __next__(self):
-#         # if sum(self.state) > self.val:
-#         #     raise StopIteration
-#
-#         for i in range(len(self.set) + 1):
-#             for comb in itertools.combinations(set(self.set), i):
-#                 if sum(comb) <= self.val:
-#                     res = self.state
-#                     self.state = list(comb)
-#                     return res
-#                 else:
-#                     raise StopIteration
-#
 # ===================== Q1.a ================== #
 def bounded_subsets(bound_set, val, mem=[]):
+    """
+     >>> for j in bounded_subsets([1, 2, 4], 1): print (j, end="")
+     [][1]
+     >>> for j in bounded_subsets([7,8,9,10], 20): print (j, end="")
+     [7][8][9][10][7, 8][7, 9][7, 10][8, 9][8, 10][9, 10]
+     >>> for j in bounded_subsets(range(5,15), 15): print (j, end="")
+     [5][6][11][12][13][14][5, 6][5, 7][5, 8][5, 9][5, 10][6, 7][6, 8][6, 9]
+    """
     for i in range(len(bound_set) + 1):
-        for comb in itertools.combinations(bound_set, i):
-            if sum(comb) <= val:
-                if comb not in mem:
+        for comb in itertools.combinations(bound_set, i):  # iterate over all the combinations
+            if sum(comb) <= val:  # check if sum of the combination is lower than val
+                if comb not in mem:  # check in mem so we dont check the same comb twice
                     mem.append(comb)
-                    yield list(comb)
-
-
-# for s in bounded_subsets([1, 2, 3], 4):
-#     print(s)
-
-# for s in bounded_subsets(range(50, 150), 103):
-#     print(s)
-
-# for s in zip(range(5), bounded_subsets(range(100), 1000000000000)):
-#     print(s)
+                    yield list(comb)  # will remember the last comb
 
 
 # ===================== Q1.b ================== #
@@ -57,4 +32,7 @@ def bounded_subsets(bound_set, val, mem=[]):
 # for s in ordered_bounded_subsets([1, 2, 3], 4):
 #     print(s)
 
+if __name__ == "__main__":
+    import doctest
 
+    print(doctest.testmod())
---
+++ Ex5/Q2.py
@@ -0,0 +1,162 @@
+import math
+import sys
+from typing import Callable
+
+graph1 = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+          [4, 0, 8, 0, 0, 0, 0, 11, 0],
+          [0, 8, 0, 7, 0, 4, 0, 0, 2],
+          [0, 0, 7, 0, 9, 14, 0, 0, 0],
+          [0, 0, 0, 9, 0, 10, 0, 0, 0],
+          [0, 0, 4, 14, 10, 0, 2, 0, 0],
+          [0, 0, 0, 0, 0, 2, 0, 1, 6],
+          [8, 11, 0, 0, 0, 0, 1, 0, 7],
+          [0, 0, 2, 0, 0, 0, 6, 7, 0]
+          ]
+graph2 = {0: [(0, 0), (1, 5), (2, math.inf), (3, math.inf)],
+          1: [(0, 50), (1, 0), (2, 15), (3, 5)],
+          2: [(0, 30), (1, math.inf), (2, 0), (3, 15)],
+          3: [(0, 15), (1, math.inf), (2, 5), (3, 0)]
+          }
+
+
+def same_look(graph :dict):
+    """
+    this function gets a graph and if the graph is a dictionary it turns it to a matrix. in this way the algo can accepts two ways of input
+    """
+    if type(graph) is list:
+        return graph
+    rows = cols = len(graph.keys())
+    i = j = 0
+    mat = [[0 for _ in range(cols)] for _ in range(rows)]
+    while i != rows:
+        for adj in graph.values():
+            for val in adj:
+                mat[i][j] = val[1]
+                j += 1
+            i += 1
+            j = 0
+    return mat
+
+
+class Output:
+    def sum(self):
+        """
+         sums the length of shortest paths
+        """
+        sum = 0
+        for i in self:
+            sum += i
+        return sum
+
+    def dist_from_source(self):
+        """
+        find the distance of the shortest paths from source vertex to all other vertices
+        """
+        res = dict()
+        for node in range(len(self)):
+            res[node] = self[node]
+        return res
+
+
+def path_algo(algorithm: Callable, items: list, src, outputtype=Output):
+    """
+    >>> path_algo(algorithm=dijkstra, items=graph1, src=1, outputtype=Output.dist_from_source)
+    {0: 4, 1: 0, 2: 8, 3: 15, 4: 22, 5: 12, 6: 12, 7: 11, 8: 10}
+    >>> path_algo(algorithm=dijkstra, items=graph1, src=1, outputtype=Output.sum)
+    94
+    >>> path_algo(algorithm=floyd_warshall, items=graph1, src=1, outputtype=Output.dist_from_source)
+    {0: 4, 1: 0, 2: 8, 3: 15, 4: 22, 5: 12, 6: 12, 7: 11, 8: 10}
+    >>> path_algo(algorithm=floyd_warshall, items=graph1, src=1, outputtype=Output.sum)
+    94
+    >>> path_algo(algorithm=dijkstra, items=graph2, src=2, outputtype=Output.dist_from_source)
+    {0: 30, 1: 35, 2: 0, 3: 15}
+    >>> path_algo(algorithm=dijkstra, items=graph2, src=2, outputtype=Output.sum)
+    80
+    >>> path_algo(algorithm=floyd_warshall, items=graph2, src=2, outputtype=Output.dist_from_source)
+    {0: 30, 1: 35, 2: 0, 3: 15}
+    >>> path_algo(algorithm=floyd_warshall, items=graph2, src=2, outputtype=Output.sum)
+    80
+    """
+    path = algorithm(same_look(items), src)
+    return outputtype(path)
+
+
+def minDistance(dist, sptSet, v_num):
+    # Initialize minimum distance for next node
+    min_index = 0
+    min = 1e7
+
+    # Search not nearest vertex not in the
+    # shortest path tree
+    for v in range(v_num):
+        if dist[v] < min and sptSet[v] == False:
+            min = dist[v]
+            min_index = v
+
+    return min_index
+
+
+def dijkstra(graph, src):
+    v_num = len(graph[0])
+    dist = [sys.maxsize] * v_num
+    dist[src] = 0
+    sptSet = [False] * v_num
+
+    for cout in range(v_num):
+
+        # Pick the minimum distance vertex from
+        # the set of vertices not yet processed.
+        # x is always equal to src in first iteration
+        x = minDistance(dist, sptSet, v_num)
+
+        # Put the minimum distance vertex in the
+        # shortest path tree
+        sptSet[x] = True
+
+        # Update dist value of the adjacent vertices
+        # of the picked vertex only if the current
+        # distance is greater than new distance and
+        # the vertex in not in the shortest path tree
+        for y in range(v_num):
+            if graph[x][y] > 0 and sptSet[y] == False and \
+                    dist[y] > dist[x] + graph[x][y]:
+                dist[y] = dist[x] + graph[x][y]
+    return dist
+
+
+def floyd_warshall(graph, src):
+    graph = change_to_inf(graph)
+    v_num = len(graph[0])
+    distance = list(map(lambda i: list(map(lambda j: j, i)), graph))
+
+    # Adding vertices individually
+    for k in range(v_num):
+        for i in range(v_num):
+            for j in range(v_num):
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+    return distance[src]
+
+
+def change_to_inf(graph):
+    """
+     if there is no path between two vertices, there will need to be inf in the place of the matrix for floyd_warshall to watk
+    """
+    v_num = len(graph[0])
+    for i in range(v_num):
+        for j in range(v_num):
+            if graph[i][j] == 0 and i != j:
+                graph[i][j] = math.inf
+    return graph
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testmod())
+    print(path_algo(algorithm=dijkstra, items=graph1, src=0, outputtype=Output.dist_from_source))
+    print(path_algo(algorithm=dijkstra, items=graph1, src=0, outputtype=Output.sum))
+    print(path_algo(algorithm=floyd_warshall, items=graph1, src=0, outputtype=Output.dist_from_source))
+    print(path_algo(algorithm=floyd_warshall, items=graph1, src=0, outputtype=Output.sum))
+    print(path_algo(algorithm=dijkstra, items=graph2, src=0, outputtype=Output.dist_from_source))
+    print(path_algo(algorithm=dijkstra, items=graph2, src=0, outputtype=Output.sum))
+    print(path_algo(algorithm=floyd_warshall, items=graph2, src=0, outputtype=Output.dist_from_source))
+    print(path_algo(algorithm=floyd_warshall, items=graph2, src=0, outputtype=Output.sum))
